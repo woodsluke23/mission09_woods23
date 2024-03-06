f1f6413f4a12aa26940fdb8ce5e508e4e74150a9
Luke Woods <woodsluke23@gmail.com>
2024-03-06 13:16:09 -0700

    this is my amazing project

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import data from "./CollegeBasketballTeams.json";
+
+interface TeamBlah {
+  school: string;
+  name: string;
+  city: string;
+  state: string;
+}
+
+function Welcome() {
+  return <h1> NCAA Basketball Team Information! </h1>;
+}
+
+class Team extends React.Component<TeamBlah> {
+  render() {
+    const oneTeam = this.props;
+
+    return (
+      <div>
+        <h2> School Name: {oneTeam.school} </h2>
+        <h2> Mascot Name: {oneTeam.name} </h2>
+        <h2>
+          Location: {oneTeam.city}, {oneTeam.state}
+        </h2>
+        <br></br>
+        <br></br>
+      </div>
+    );
+  }
+}
+
+function TeamList() {
+  return (
+    <div>
+      {data.teams.map((teamNum) => (
+        <Team {...teamNum} />
+      ))}
+    </div>
+  );
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Welcome />
+      <TeamList />
     </div>
   );
 }
